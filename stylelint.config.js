79fc2c1ccc18a552e8011d6885bc440f0f136b86
Harry Tran <nhattq.coding@gmail.com>
2020-04-21 12:39:25 +0700

    workflow: update rules for stylelint css

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,9 +1,16 @@
+const tailwindRules = [
+  true,
+  {
+    ignoreAtRules: ['tailwind', 'apply', 'screen', 'variants', 'responsive']
+  }
+]
+
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
   plugins: ['stylelint-scss'],
   rules: {
-    'at-rule-no-unknown': null,
-    'scss/at-rule-no-unknown': true,
+    'at-rule-no-unknown': tailwindRules,
+    'scss/at-rule-no-unknown': tailwindRules,
     'selector-pseudo-element-colon-notation': 'single',
     'comment-empty-line-before': 'never'
   }
