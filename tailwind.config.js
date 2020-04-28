4d97754bb1ea03eb79bb9f301d6fcff1ab4b8931
Restyled.io <commits@restyled.io>
2020-04-28 00:59:07 +0000

    Restyled by clang-format

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,11 @@
  ** TailwindCSS Configuration File
  **
  ** Docs: https://tailwindcss.com/docs/configuration
- ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ ** Default:
+ *https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
  */
 module.exports = {
-  theme: {},
-  variants: {},
-  plugins: []
+  theme : {},
+  variants : {},
+  plugins : []
 }
