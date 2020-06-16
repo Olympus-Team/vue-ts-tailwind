bac105c05b2fdb2537d1f5d5ac1645aed94578fc
Restyled.io <commits@restyled.io>
2020-06-16 06:36:09 +0000

    Restyled by prettier

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -6,16 +6,13 @@
  *https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
  */
 module.exports = {
-  theme : {},
-  variants : {},
-  plugins : [],
-  purge : {
+  theme: {},
+  variants: {},
+  plugins: [],
+  purge: {
     // Learn more on
     // https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-    enabled : process.env.NODE_ENV === 'production',
-    content : [
-      'components/**/*.vue', 'layouts/**/*.vue', 'pages/**/*.vue',
-      'plugins/**/*.js', 'nuxt.config.js'
-    ]
+    enabled: process.env.NODE_ENV === 'production',
+    content: ['components/**/*.vue', 'layouts/**/*.vue', 'pages/**/*.vue', 'plugins/**/*.js', 'nuxt.config.js']
   }
 }
